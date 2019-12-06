ac85553439ebf8cf71b0a2e954ffefffa42fd638
Shiying Wang <fsywang@alumni.ubc.ca>
2019-12-06 13:31:19 -0800

    change the line to percentage

--- app.py
+++ app.py
@@ -17,11 +17,11 @@ server = app.server
 # set app title
 app.title = 'Dash app with pure Altair HTML'
 
-# import, wrangle and preparation 
+## import, wrangle and preparation 
 # source: https://www.naturalearthdata.com/downloads/110m-cultural-vectors/
 # source:https://towardsdatascience.com/a-complete-guide-to-an-interactive-geographical-map-using-python-f4c5197e23e0
 factors_data = pd.read_csv('data/clean_data_line_plot.csv')
-factors_data.iloc[:,2:] = factors_data.iloc[:,2:]/1_000_000
+factors_data.iloc[:,2:] = factors_data.iloc[:,2:].div(factors_data.iloc[:,2:].sum(axis=1), axis=0)
 
 df = pd.read_csv("data/clean_number-of-deaths-by-risk-factor.csv")
 shapefile = 'data/geographic_data/ne_110m_admin_0_countries.shp'
@@ -36,9 +36,9 @@ geo_df.iloc[:,4:] = geo_df.iloc[:,4:].div(geo_df.iloc[:,4:].sum(axis=1), axis=0)
 # convert to json file
 choro_json = json.loads(geo_df.to_json())
 choro_data = alt.Data(values=choro_json['features'])
-# wrangling ends
+## wrangling ends
 
-# draw_map() function
+## draw_map() function
 # source: https://medium.com/dataexplorations/creating-choropleth-maps-in-altair-eeb7085779a1 
 def draw_map(cols = 'properties.high_blood_pressure', source = choro_data):
     """
@@ -67,10 +67,10 @@ def draw_map(cols = 'properties.high_blood_pressure', source = choro_data):
                                 format = ".2%")]
     ).properties(width=800, height=450).configure_title(fontSize=25)
     return  p_map
-# draw map function ends
+## draw map function ends
 
 
-# line_graph function
+## line_graph function
 selection = alt.selection_single()
 
 def line_graph(factor_name = 'high_blood_pressure', data = factors_data):
@@ -88,14 +88,15 @@ def line_graph(factor_name = 'high_blood_pressure', data = factors_data):
     line = alt.Chart(data).mark_line(point=True).add_selection(selection
 ).encode(
     alt.X("year:N", axis=alt.Axis(labelAngle=45)),
-    alt.Y("{}:Q".format(factor_name),title="Number of death (in million)"),
+    alt.Y("{}:Q".format(factor_name),title="Percentage of death over total death",
+          axis=alt.Axis(format='%') ),
     tooltip = ['year:N','continent:N', 
-               alt.Tooltip('{}:Q'.format(factor_name), format = ".2f",
-                          title="Number of death (in million)")],
+               alt.Tooltip('{}:Q'.format(factor_name), format = ".2%",
+                          title="percentage of death")],
     color = alt.condition(selection, 'continent:N', alt.value('grey')),
     opacity = alt.condition(selection, alt.value(0.9), alt.value(0.2))
 ).properties(
-    title="Trend of {} over time across continents , 1990 - 2017".format(factor_name.replace("_"," ")),
+    title="Trend of death due to {} over time , 1990 - 2017".format(factor_name.replace("_"," ")),
     width=800,
     height=350).configure_title(fontSize=25
     ).configure_axis(
@@ -103,9 +104,9 @@ def line_graph(factor_name = 'high_blood_pressure', data = factors_data):
     titleFontSize=20
 )
     return line 
-# line_graph function ends
+## line_graph function ends
 
-# jumbotron starts
+## jumbotron starts
 jumbotron = dbc.Jumbotron(
     [
         html.H1("Worldwide Death Risk Factors", className="display-3"),
@@ -114,10 +115,10 @@ jumbotron = dbc.Jumbotron(
         ),
     ]
 )
-# jumbotron ends
+## jumbotron ends
 
 
-# layout starts
+## layout starts
 app.layout = html.Div([jumbotron,
     dcc.Tabs([
         # first tab starts
@@ -193,7 +194,7 @@ app.layout = html.Div([jumbotron,
                 width='1000',
                 style={'border-width': '0'},
 
-                ## call line_graph() function
+                # call line_graph() function
                 srcDoc=line_graph().to_html()
                 ),
 
@@ -207,9 +208,9 @@ app.layout = html.Div([jumbotron,
         }),
         html.Div(id='tabs-content-props')
 ], style={'textAlign':'center'})
-# layout ends
+## layout ends
 
-# setting callbacks
+## setting callbacks
 @app.callback(
     dash.dependencies.Output('plot_map', 'srcDoc'),
     [dash.dependencies.Input('dd-chart', 'value')])
@@ -231,7 +232,7 @@ def update_line_graph(factor_name):
     '''
     updated_line_graph = line_graph(factor_name).to_html()
     return updated_line_graph
-# setting call back ends
+## setting call back ends
 
 if __name__ == '__main__':
     app.run_server(debug=True)
